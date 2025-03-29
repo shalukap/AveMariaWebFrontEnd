8275a482c9573de63a7d2cafb25cb6b2a6726d3e
shalukap <jude.shaluka.perera@gmail.com>
2025-03-29 17:36:01 +0530

    homa image update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import News from '../components/news'
 import Calender from '../components/calender'
 import Vision from '../components/vision'
 import NavBar from '../components/navbar'
+import './home.css'
 
 export default function Home() {
   return (
     <div className='h-[100%] w-full  bg-primary'>
     
-      <div className="relative h-screen w-full bg-[url('./assets/img/001.png')] bg-cover shadow-[0px_4px_10px_rgba(255,255,255,0.8)] flex flex-col items-center justify-center ">
+      <div className="relative h-screen w-full bg-image-home shadow-[0px_4px_10px_rgba(255,255,255,0.8)] flex flex-col items-center justify-center ">
       
       <NavBar/> 
         
