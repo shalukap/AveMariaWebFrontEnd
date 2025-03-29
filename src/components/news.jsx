ef1a6c1bdef9450a3cf0b3def9d0f7389b6dd720
shalukap <jude.shaluka.perera@gmail.com>
2025-03-29 18:53:34 +0530

    arrenging all components

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -30,11 +30,11 @@ export default function News() {
       <h1 className='sm:text-7xl md:text-7xl font-bold font-Amaranth p-5 text-white'>
         Latest News
       </h1>
-      <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-10 p-4'>
+      <div className='grid md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 gap-10 p-4'>
         {news.slice(0,4).map((newsItem) => (
-          <div key={newsItem.nid} className="card bg-base-100 image-full w-full shadow-xl overflow-hidden">
+          <div key={newsItem.nid} className="card bg-base-100 image-full w-full h-96 shadow-xl overflow-hidden">
             <figure>
-              <img src={newsItem.images && newsItem.images.length > 0 ? newsItem.images[0] : "/assets/img/default.jpg"} alt={newsItem.title} />
+              <img src={newsItem.images && newsItem.images.length > 0 ? newsItem.images[0] : "/assets/img/default.jpg"} alt={newsItem.title} className='w-full h-full cover'/>
             </figure>
             <div className="card-body relative">
               <h2 className="card-title">{newsItem.title}</h2>
