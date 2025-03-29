11892e4b0db4d81dee579cdff109612ad58bcb23
shalukap <jude.shaluka.perera@gmail.com>
2025-03-29 17:25:22 +0530

    imageCss change

--- src/components/vision.jsx
+++ src/components/vision.jsx
@@ -5,7 +5,7 @@ export default function Vision() {
   return (
     <div className='h-screen w-full flex'>
       
-      <div className="relative h-screen sm:w-full md:w-1/2 bg-image">
+      <div className="relative h-screen sm:w-full md:w-1/2 bg-image-vision">
       <div className="absolute inset-0 bg-[radial-gradient(circle,transparent,theme(colors.primary)_80%)]"></div>
 
       </div>
